761e293a97f2357f10bad395fd6c59a1dd4de221
3commascapital <3commascapital@gmail.com>
2021-09-30 12:08:33 -0500

    trying to check proto again

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,25 +1,24 @@
 package server
 
 import (
-	"fmt"
 	"net/http"
+	"os"
 )
 
 type Redirect struct{}
 
 func (redirect *Redirect) ServeHTTP(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-	fmt.Printf("proto: %s\n", req.Header.Get("X-Forwarded-Proto"))
-	next(w, req)
-	// if os.Getenv("ENV") != "production" || req.Header.Get("X-Forwarded-Proto") == "https:" {
-	// 	next(w, req)
-	// 	return
-	// }
-	// // remove/add not default ports from req.Host
-	// target := "https://" + req.Host + req.URL.Path
-	// if len(req.URL.RawQuery) > 0 {
-	// 	target += "?" + req.URL.RawQuery
-	// }
-	// http.Redirect(w, req, target, http.StatusTemporaryRedirect)
+	proto := req.Header.Get("X-Forwarded-Proto")
+	if os.Getenv("ENV") != "production" || proto == "https" {
+		next(w, req)
+		return
+	}
+	// remove/add not default ports from req.Host
+	target := "https://" + req.Host + req.URL.Path
+	if len(req.URL.RawQuery) > 0 {
+		target += "?" + req.URL.RawQuery
+	}
+	http.Redirect(w, req, target, http.StatusTemporaryRedirect)
 }
 
 func NewRedirect() *Redirect {
