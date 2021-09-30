3b8b4a1192d3d77071c61e4280cd89d94489c034
3commascapital <3commascapital@gmail.com>
2021-09-30 11:43:57 -0500

    added log

--- internal/server/middleware.go
+++ internal/server/middleware.go
@@ -1,23 +1,27 @@
 package server
 
 import (
+	"fmt"
 	"net/http"
-	"os"
 )
 
 type Redirect struct{}
 
 func (redirect *Redirect) ServeHTTP(w http.ResponseWriter, req *http.Request, next http.HandlerFunc) {
-	if os.Getenv("ENV") != "production" || req.Proto == "https:" {
-		next(w, req)
-		return
-	}
-	// remove/add not default ports from req.Host
-	target := "https://" + req.Host + req.URL.Path
-	if len(req.URL.RawQuery) > 0 {
-		target += "?" + req.URL.RawQuery
-	}
-	http.Redirect(w, req, target, http.StatusTemporaryRedirect)
+	// env := os.Getenv("ENV")
+	fmt.Println(req.URL.Scheme)
+	next(w, req)
+	// if env != "production" || req.URL.Scheme == "https" {
+	// 	fmt.Printf("scheme %#v\n", req.Header.Get("origin"))
+	// 	next(w, req)
+	// 	return
+	// }
+	// // remove/add not default ports from req.Host
+	// target := "https://" + req.Host + req.URL.Path
+	// if len(req.URL.RawQuery) > 0 {
+	// 	target += "?" + req.URL.RawQuery
+	// }
+	// http.Redirect(w, req, target, http.StatusTemporaryRedirect)
 }
 
 func NewRedirect() *Redirect {
